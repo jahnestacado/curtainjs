57795c9e31f2185ece2544b6ef8f3dc0e4c467b0
Jahn <ioannis.kiwe@gmail.com>
2015-12-05 03:25:04 +0100

    Init commit

---
+++ js/curtain.js
@@ -0,0 +1,174 @@
+(function ($) {
+    var OPTIONS = {
+        initState: null,
+        defaultMode: "landscape",
+        swapModeOnOrientationChange: false,
+        animationDuration: 360,
+        animationEasing: "swing",
+    };
+
+    var CurtainJs = function curtainJs(parentSelector, _options) {
+        var parentElQ = $(parentSelector);
+        var options = $.extend( {}, OPTIONS, _options);
+        var isPortrait =  utils.getCurrentMode(options) === "portrait";
+
+        var positioning = utils.getCurtainPositioning(parentElQ, options);
+        var parentDimensions = utils.getParentDims(parentElQ);
+
+        var alphaCurtainElQ = utils.renderAlphaCurtain(isPortrait, positioning, parentDimensions);
+        var betaCurtainElQ =  utils.renderBetaCurtain(isPortrait, positioning, parentDimensions);
+
+        parentElQ
+                .append([alphaCurtainElQ, betaCurtainElQ])
+                .attr({id: options.id})
+                .css({overflow: "hidden"});
+
+        var controls = {
+            close: function close(withoutAnimation) {
+                var positioning =  utils.getCurtainPositioning(parentElQ, options);
+                var animationDuration = withoutAnimation ? 0 : options.animationDuration;
+
+                alphaCurtainElQ.removeClass("curtain-A-opened").addClass("curtain-A-closed");
+                betaCurtainElQ.removeClass("curtain-B-opened").addClass("curtain-B-closed");
+
+                var alphaCurtainPositioning = isPortrait
+                                    ? {top :"-" + positioning.top + "px"}
+                                    : {left : '-' + positioning.left + 'px'};
+                alphaCurtainElQ.animate(alphaCurtainPositioning, animationDuration, options.animationEasing, function(){alphaCurtainElQ.css({display: "none"});});
+
+                var betaCurtainPositioning = isPortrait
+                                    ? {bottom : "-" + positioning.bottom + "px"}
+                                    : {right : '-' + positioning.right + 'px'};
+                betaCurtainElQ.animate(betaCurtainPositioning, animationDuration, options.animationEasing, function(){betaCurtainElQ.css({display: "none"});});
+            },
+            open: function open() {
+                var positioning = utils.getCurtainPositioning(parentElQ, options);
+                var parentDimensions = utils.getParentDims(parentElQ);
+
+                alphaCurtainElQ =  utils.renderAlphaCurtain(isPortrait, positioning, parentDimensions, alphaCurtainElQ);
+                betaCurtainElQ =  utils.renderBetaCurtain(isPortrait, positioning, parentDimensions, betaCurtainElQ);
+                parentElQ.append([alphaCurtainElQ, betaCurtainElQ]);
+
+                alphaCurtainElQ.removeClass("curtain-A-closed")
+                                .addClass("curtain-A-opened")
+                                .css({display: ""});
+
+                betaCurtainElQ.removeClass("curtain-B-closed")
+                                .addClass("curtain-B-opened")
+                                .css({display: ""});
+
+                var alphaCurtainPositioning = isPortrait ? {top: "0"} : {left: "0"};
+                alphaCurtainElQ.animate(alphaCurtainPositioning, options.animationDuration, options.animationEasing);
+
+                var betaCurtainPositioning = isPortrait ? {bottom: "0"} : {right: "0",};
+                betaCurtainElQ.animate(betaCurtainPositioning, options.animationDuration, options.animationEasing);
+            },
+            curtainA: alphaCurtainElQ,
+            curtainB: betaCurtainElQ
+        };
+
+        var isInFullscreenMode = parentElQ.width() >= $(window).width() || parentElQ.height() >= $(window).height();
+        if(isInFullscreenMode){
+            $(window).on("resize", function () {
+                controls.close(true);
+                isPortrait =  utils.getCurrentMode(options) === "portrait";
+            });
+        }
+
+        if(options.initState === "active"){
+            controls.open();
+        }
+
+        return $.extend({},parentElQ, controls);
+    };
+
+    var utils = {
+        getCurrentMode : function getCurrentMode(options){
+            var self = this;
+            var mode = options.defaultMode;
+            if( options.portrait && options.landscape && options.swapModeOnOrientationChange){
+                var isInPortraitMode =  utils.isOrientationPortrait();
+                switch(options.defaultMode){
+                    case "landscape":
+                            mode = isInPortraitMode ? "portrait" : options.defaultMode;
+                            break;
+                    case "portrait":
+                        mode = isInPortraitMode ? "landscape" : options.defaultMode;
+                        break;
+                    default:
+                        console.log("Unkonwn orientation mode" + defaultMode);
+                    break;
+                }
+            } else if(options.portrait && !options.landscape){
+                mode = "portrait";
+            }
+            return mode;
+        },
+        renderAlphaCurtain : function renderAlphaCurtain(isPortrait, positioning, parentDimensions, existingElQ) {
+            var self = this;
+            alphaCurtainElQ = existingElQ || $("<div class='curtain-A'></div>");
+            var alphaCurtainBorderSize = self.getBorderSize(alphaCurtainElQ);
+
+            var cssAttrs = isPortrait
+                                    ? {top: "-" + positioning.top + "px",  left: "0", height:positioning.top, width: parentDimensions.innerWidth + "px"}
+                                    : {left: "-" + positioning.left + "px", top: "0", width: positioning.left, height: parentDimensions.height +"px"};
+            cssAttrs.border = alphaCurtainBorderSize + "px solid black";
+
+            alphaCurtainElQ.addClass("curtain-A-closed").css(cssAttrs);
+
+            return alphaCurtainElQ;
+        },
+        renderBetaCurtain : function renderBetaCurtain(isPortrait, positioning, parentDimensions, existingElQ) {
+            var self = this;
+            betaCurtainElQ = existingElQ || $("<div class='curtain-B'></div>");
+            var betaCurtainBorderSize = self.getBorderSize(betaCurtainElQ);
+
+            var cssAttrs = isPortrait
+                                    ? {bottom: "-" + 2 * positioning.bottom + "px",  right: "0", height: positioning.bottom,  width: parentDimensions.innerWidth + "px"}
+                                    :{right: "-" + 2 * positioning.right + "px", bottom: "0", width: positioning.right, height: parentDimensions.height + "px"};
+            cssAttrs.border = betaCurtainBorderSize + "px solid black";
+            betaCurtainElQ.addClass("curtain-B-closed").css(cssAttrs);
+
+            return betaCurtainElQ;
+        },
+        getBorderSize : function getBorderSize(parentElQ){
+            var self = this;
+            var sreenWidths = self.getParentDims(parentElQ);
+            var borderSize = (sreenWidths.outerWidth - sreenWidths.innerWidth) / 2;
+            return borderSize;
+        },
+        getParentDims : function getParentDims(parentElQ){
+            var screenWidths = {
+                outerWidth : parentElQ.outerWidth() || parentElQ.width(),
+                innerWidth :  parentElQ.innerWidth() || parentElQ.width(),
+                height : parentElQ.is("body") ? $(window).height() : parentElQ.height(),
+            };
+
+            return screenWidths;
+        },
+        getCurtainPositioning : function getCurtainPositioning(parentElQ, options) {
+            var self = this;
+            var currentMode = self.getCurrentMode(options);
+            var alphaCurtainScreenRatio = options[currentMode] && options[currentMode].screenRatioCurtainA || 0;
+            var betaCurtainScreenRatio =  options[currentMode] && options[currentMode].screenRatioCurtainB || 0;
+            var innerWidth = self.getParentDims(parentElQ).innerWidth;
+            var height = self.getParentDims(parentElQ).height;
+
+            var alphaCurtainBorderSize = self.getBorderSize(parentElQ.find(".curtain-A"));
+            var betaCurtainBorderSize = self.getBorderSize(parentElQ.find(".curtain-B"));
+
+            var curtainPositioning = {
+                left : innerWidth * alphaCurtainScreenRatio - alphaCurtainBorderSize * 2,
+                right : innerWidth * betaCurtainScreenRatio - betaCurtainBorderSize * 2,
+                top : height * alphaCurtainScreenRatio - alphaCurtainBorderSize * 2,
+                bottom : height * betaCurtainScreenRatio - betaCurtainBorderSize * 2
+            };
+            return curtainPositioning;
+        },
+        isOrientationPortrait : function isOrientationPortrait(){
+            return $(window).innerWidth() < $(window).innerHeight();
+        }
+    };
+
+    $.curtain = CurtainJs;
+})(jQuery);
