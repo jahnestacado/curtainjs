795082013ac3239baaf55b890f15175fe163cc6f
Jahn <ioannis.kiwe@gmail.com>
2015-12-05 21:41:35 +0100

    js/curtain.js: Made script AMD/Node/CommonJs compatible

--- js/curtain.js
+++ js/curtain.js
@@ -1,4 +1,32 @@
-(function ($) {
+//https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     var OPTIONS = {
         initState: null,
         defaultMode: "landscape",
@@ -7,8 +35,8 @@
         animationEasing: "swing",
     };
 
-    var CurtainJs = function curtainJs(parentContainer, _options) {
-        var parentElQ = parentContainer.jquery ? parentContainer : $(parentContainer);
+    var CurtainJs = function CurtainJs(_options) {
+        var parentElQ = this;
         var options = $.extend( {}, OPTIONS, _options);
         var positioning = utils.getCurtainPositioning(parentElQ, options);
         var parentDimensions = utils.getElementsDims(parentElQ);
@@ -174,5 +202,5 @@
         }
     };
 
-    $.curtain = CurtainJs;
-})(jQuery);
+    $.fn.curtain = CurtainJs;
+}));
